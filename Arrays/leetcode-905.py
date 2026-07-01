e8389cdb89d9c9ff01c0c4bed3c1e071539ea5a0
shravan6778 <jaligamshravan2004@gmail.com>
2026-07-01 06:09:07 +0530

    LeetCode-905 Sort array by parity

--- Arrays/leetcode-905.py
+++ Arrays/leetcode-905.py
@@ -1,7 +1,7 @@
 '''Input: nums = [3,1,2,4]
 Output: [2,4,3,1]
 Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.'''
-
+from typing import List
 def sortArrayByParity(nums: List[int]) -> List[int]:
     start=0
     for i in range(0,len(nums)):
